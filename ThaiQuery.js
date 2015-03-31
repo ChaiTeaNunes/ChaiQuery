60f228d5319f068fb1af5db39a41b8dd4c83e56b
Chaiyawat Nunes <thaiberius.code@gmail.com>
2015-03-30 21:40:06 -0700

    quick edits

--- ThaiQuery.js
+++ ThaiQuery.js
@@ -23,7 +23,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-/* */
+/* OBJECT CONSTRUCTOR */
 function _(id) {
 	var about = {
 		Version: 0.1,
@@ -43,28 +43,33 @@ function _(id) {
 	}
 };
 
+/* PROTOTYPE FUNCTIONS */
 _.prototype = {
-	// Hide the element
+	// Hide the Element
 	hide: function() {
 		this.obj.style.display = 'none';
 		return this;
 	},
-	// Show the element
+
+	// Show the Element
 	show: function() {
 		this.obj.style.display = 'inherit';
 		return this;
 	},
-	// Change the element's background color
+
+	// Change the Element's Background Color
 	bgcolor: function(color) {
 		this.obj.style.background = color;
 		return this;
 	},
-	// Change the element's text color
+
+	// Change the Element's Text Color
 	txtcolor: function(color) {
 		this.obj.style.color = color;
 		return this;
 	},
-	// Toggle visibility the element
+
+	// Toggle Visibility of the Element
 	toggle: function(){
 		if(this.obj.style.display !== 'none') {
 			this.obj.style.display = 'none';
@@ -73,56 +78,107 @@ _.prototype = {
 		}
 		return this;
 	},
-	// Resize the element (px)
+
+	// Resize the Element (px)
 	sizepx: function(widthpx, heightpx) {
 		this.obj.style.width = width + 'px';
 		this.obj.style.height = height + 'px';
 		return this;
 	},
-	// Resize the element (%)
+
+	// Resize the Element (%)
 	sizepc: function(widthpc, heightpc) {
 		this.obj.style.width = width + '%';
 		this.obj.style.height = hieght + '%';
 		return this;
 	},
-	// Border color
+
+	// Border Style
 	border: function(width, style, color) {
 		this.obj.style.border = width +' ' + style + ' ' + color;
 		return this;
 	},
-	// Border radius
+
+	// Border Radius
 	borderRadius: function(radpx) {
 		this.obj.style.borderRadius = radpx + 'px';
 		return this;
 	},
+
 	// Margin (px)
 	marginpx: function(top, right, bottom, left) {
 		this.obj.style.margin = top + 'px ' + right + 'px ' + bottom + 'px ' + left + 'px';
 		return this; 
 	},
+
 	// Margin (%)
 	marginpc: function(top, right, bottom, left) {
 		this.obj.style.margin = top + '% ' + right + '% ' + bottom + '% ' + left + '%';
 		return this; 
 	},
+
 	// Padding (px)
 	paddingpx: function(top, right, bottom, left) {
 		this.obj.style.padding = top + 'px ' + right + 'px ' + bottom + 'px ' + left + 'px';
 		return this; 
 	},
+
 	// Padding (%)
 	paddingpc: function(top, right, bottom, left) {
 		this.obj.style.padding = top + '% ' + right + '% ' + bottom + '% ' + left + '%';
 		return this; 
 	},
-	//Toggle resize
-	toggleResize: function(value){
+
+	//Toggle Resize
+	toggleResize: function(value) {
 		this.obj.style.resize = value;
 		return this;
 	},
 
+	// Cursor Type
+	cursor: function(value) {
+		this.obj.style.cursor = value;
+		return this;
+	},
+
+	// Aligning text
+	txtAlign: function(align) {
+		this.obj.style.textAlign(align);
+		return this;
+	},
+
+	// Range of the Size for the Element (px)
+	sizerangepx: function(minWidth, minHeight, maxWidth, maxHeight) {
+		this.obj.style.minWidth = minWdith + 'px';
+		this.obj.style.minHeight = minHeight + 'px';
+		this.obj.style.maxWidth = maxWidth + 'px';
+		this.obj.style.maxHeight = maxHeight + 'px';
+		return this;
+	},
+
+	// Range of the Size for the Element (%)
+	sizerangepc: function(minWidth, minHeight, maxWidth, maxHeight) {
+		this.obj.style.minWidth = minWdith + '%';
+		this.obj.style.minHeight = minHeight + '%';
+		this.obj.style.maxWidth = maxWidth + '%';
+		this.obj.style.maxHeight = maxHeight + '%';
+		return this;
+	},
+
+	// Change Image Src
+	imgchange: function(srcImg) {
+		this.obj.style.src = srcImg;
+		return this;
+	},
+
+	// Change Text Case
+	textCase: function(case) {
+		this.obj.style.textTransform = case;
+		return this;
+	},
+
 	// Default
-	default: function() {
+	default: function(args) {
 		// this.obj.style.[css] = args;
 		return this;
 	}
